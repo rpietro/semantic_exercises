d6280a202c436e9b46a063b6c3fd7d840042845a
lucasxteixeira <lucasxteixeira@gmail.com>
2014-08-11 12:09:35 -0300

    Revert "Create test.py"
    This reverts commit 012221f0d1883204a056c4ee5ec9e52100c57bff.

--- test/test.py
+++
@@ -1,9 +0,0 @@
-import jsonld
-import json
-
-data = json.load( open('diseases.jsonld'))
-
-print data['@context']
-print data['@graph']
-
-compacted = jsonld.compact( data['@graph'] , data['@context'] )
